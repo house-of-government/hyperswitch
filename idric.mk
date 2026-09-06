4acd79ff3a099756bfee02962c469b2bc751d51d
i <isomorphisms@sdf.org>
2026-09-06 11:39:37 -0400

    Run all current Idriç acceptance checks

--- idric.mk
+++ idric.mk
@@ -8,6 +8,8 @@ all:
 test: all
 	$(IDRIC) tests/AttemptStatusTests.idric -o hyperswitch-attempt-tests
 	./build/exec/hyperswitch-attempt-tests
+	$(IDRIC) tests/PaymentMethodTests.idric -o hyperswitch-payment-method-tests
+	./build/exec/hyperswitch-payment-method-tests
 	$(IDRIC) tests/ConnectorTests.idric -o hyperswitch-connector-tests
 	./build/exec/hyperswitch-connector-tests
 
